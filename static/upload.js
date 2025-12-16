086bf4569120f3244ede0888e6f511cd1b4d5df9
PRV <romanpotapov900@gmail.com>
2025-12-16 16:19:13 +0400

    скрипт на дроп зону не работает

--- static/upload.js
+++ static/upload.js
@@ -1,4 +1,4 @@
 ```javascript
 document.querySelector('.dropzone')?.addEventListener('click',()=>{
-  document.querySelector('input[type=file]').click();
+    document.querySelector('input[type=file]').click();
 });
